0c84afa9f8cab329808e9f0e5d1050428034d6bf
Irma Gerguri <irge24@student.bth.se>
2025-12-11 16:18:02 +0100

    Change navbar details

--- frontend/web/src/components/Navbar.jsx
+++ frontend/web/src/components/Navbar.jsx
@@ -4,19 +4,15 @@ export default function Navbar() {
   return (
     <nav>
       <Link to="/">Hem</Link>
-      {" | "}
       <Link to="/login">Logga in</Link>
-      {" | "}
       <Link to="/signup">Skapa konto</Link>
-      {" | "}
+
       {/* Användar-meny, dessa ska sen enbart synas när man är inloggad.
       Men kan vara här sålänge för test. */}
+
       <Link to="/profile">Profil</Link>
-      {" | "}
       <Link to="/history">Historik</Link>
-      {" | "}
       <Link to="/saldo">Saldo</Link>
-      {" | "}
       <Link to="/signout">Logga ut</Link>
 
     {/* Glöm inte att när man logga in som admin så ska en annan meny synas, kolla kravspec/SDS. 
