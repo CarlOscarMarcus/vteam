f37173273fc8b1c4b59d31fd6300ad2c95e47dec
cob <cobj24@student.bth.se>
2025-12-11 09:55:17 +0100

    flyttat testkoordinater för scooter

--- backend/db/mock/02-scooter.sql
+++ backend/db/mock/02-scooter.sql
@@ -1,6 +1,6 @@
 -- Mock scooters
 INSERT INTO scooter (battery, position_lat, position_long, status, user_id)
 VALUES
-(85, '40.7128', '-74.0060', 1, 1),   -- belongs to Joe
-(40, '34.0522', '-118.2437', 0, 2),  -- belongs to Jane
+(85, '59.3312', '18.0611', 1, 1),   -- belongs to Joe
+(40, '59.3205', '18.0621', 0, 2),  -- belongs to Jane
 (100, '51.5074', '-0.1278', 1, null);   -- unassigned
