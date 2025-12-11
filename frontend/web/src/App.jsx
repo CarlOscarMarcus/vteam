c7ebd52bcb18ad0747bebc04e08dcc1077956898
cob <cobj24@student.bth.se>
2025-12-11 14:29:55 +0100

    filer för maps på webben

--- frontend/web/src/App.jsx
+++ frontend/web/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Index from './pages/Index';
 import About from './pages/About';
+import MapPage from './pages/Map'; 
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Index />} />
         <Route path="about" element={<About />} />
+        <Route path="map" element={<MapPage />} />
       </Route>
     </Routes>
   );
