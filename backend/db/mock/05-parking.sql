dd3fa02d24fe857e1af1ef565bd7f43cf5d17ef0
cob <cobj24@student.bth.se>
2025-12-18 15:59:04 +0100

    adderade fler städer och exempel

--- backend/db/mock/05-parking.sql
+++ backend/db/mock/05-parking.sql
@@ -1,6 +1,12 @@
 -- Mock parking
 INSERT INTO parking (position_lat, position_long, status)
 VALUES
-('59.3319', '18.0616', 1),   -- parking spot 1
-('59.3207', '18.0621', 0),   -- parking spot 2
-('59.3291', '18.0685', 1);   -- parking spot 3
+('59.3319', '18.0616', 1),   -- parking spot 1 (sthlm)
+('59.3207', '18.0621', 0),   -- parking spot 2 (sthlm)
+('59.3291', '18.0685', 1);   -- parking spot 3 (sthlm)
+('55.3480', '12.5960', 1);   -- parking spot 3 (malmö)
+('55.3522', '12.5990', 0),   -- parking spot 1 (malmö)
+('55.0810', '12.1011', 1),   -- parking spot 2 (malmö)
+('57.4810', '11.6011', 1);   -- parking spot 3 (gtb)
+('57.6523', '11.5520', 1),   -- parking spot 1 (gtb)
+('57.2178', '11.3290', 0),   -- parking spot 2 (gtb)
