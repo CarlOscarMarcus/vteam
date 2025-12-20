55d5747827e26660673cb8bbe15717f5ab333700
Irma Gerguri <irge24@student.bth.se>
2025-12-20 14:51:49 +0100

    User profile for frontend/web

--- frontend/web/src/pages/profile.jsx
+++ frontend/web/src/pages/profile.jsx
@@ -1,13 +1,39 @@
 // användarprofil med information om kundens konto
 
-export default function Profile() {
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { useAuth } from "../context/UserContext"
 
+export default function Profile() {
+  const navigate = useNavigate()
+  const { user, loggedIn, loadingUser } = useAuth()
+
+  useEffect(() => {
+    if (!loadingUser && !loggedIn) {
+      navigate("/login")
+    }
+  }, [loggedIn, loadingUser, navigate])
+
+  if (loadingUser) {
+    return <p>Laddar användare...</p>
+  }
+
+  if (!user) {
+    return <p>Kunde inte ladda användare</p>
+  }
 
   return (
-    <>
-      <div>
-        <h1> Användarprofil</h1>
-      </div>
-    </>
+    <div>
+      <h1>Användarprofil</h1>
+
+      <p><strong>Namn:</strong> {user.name}</p>
+      <p><strong>E-post:</strong> {user.email}</p>
+
+      <hr />
+
+      {/* <p>🛴 Resor</p>
+      <p>💰 Saldo</p>
+      <p>💸 Betala</p> */}
+    </div>
   )
 }
