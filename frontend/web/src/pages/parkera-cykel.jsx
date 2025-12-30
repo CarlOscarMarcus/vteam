1aea9ec27f63fc9977680e166e7e3778407627ff
hhannaarvid <176487087+hhannaarvid@users.noreply.github.com>
2025-12-30 14:37:13 +0100

    added file for parking scooter

---
+++ frontend/web/src/pages/parkera-cykel.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom";
+// flytta cykel till valfri parkering.
+
+
+export default function parkScooter() {
+
+
+  return (
+    <>
+      <div>
+        <h1> Historik över resor.</h1>
+      </div>
+    </>
+  )
+}
+
