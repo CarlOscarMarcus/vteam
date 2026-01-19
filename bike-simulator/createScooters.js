a044d9d58d31808eb56e2a0c80435a241bfac071
hhannaarvid <176487087+hhannaarvid@users.noreply.github.com>
2026-01-19 10:07:38 +0100

    functions for creating parkings and chargers for simulation added

--- bike-simulator/createScooters.js
+++ bike-simulator/createScooters.js
@@ -2,8 +2,19 @@ import fetch from "node-fetch";
 
 const backend_url = "http://localhost:3000";
 
-// välj antal cyklar, bäst om det är delbart med 3.
+// ####################################################### //
+// ########## // JUSTERA SIMULERINGEN // ################# //
+
+// CYKLAR, bäst om det är delbart med 3.
 const COUNT = 90;
+
+// PARKERINGAR
+const parkings = 30
+
+// LADDSTATIONER
+const chargers = 21
+
+
 const cities = [
     {
         name: "Stockholm",
@@ -43,4 +54,46 @@ async function generateScooters() {
     console.log(`${COUNT} scooters skapade, fördelade jämnt över göteborg, stockholm och malmö.`);
 }
 
+async function newParkings() {
+    for (let i = 0; i < parkings; i++) {
+        const city = cities[i % cities.length]
+        const lat = city.lat + (Math.random() - 0.5) * 0.05.toFixed(6)
+        const long = city.long + (Math.random() - 0.5) * 0.05.toFixed(6)
+
+        await fetch (`${backend_url}/api/parkings/add`, {
+            method: "POST",
+            headers: {"content-type": "application/json"},
+            body: JSON.stringify({
+                battery: Math.floor(50 + Math.random() * 51),
+                position_lat: Number(lat),
+                position_long: Number(long)
+            })
+        })
+        console.log(`Parking -> ${city.name}`)
+
+    }
+}
+
+async function newChargers() {
+    for (let i = 0; i < chargers; i++) {
+        const city = cities[i % cities.length]
+        const lat = city.lat + (Math.random() - 0.5) * 0.05.toFixed(6)
+        const long = city.long + (Math.random() - 0.5) * 0.05.toFixed(6)
+
+        await fetch (`${backend_url}/api/parkings/add`, {
+            method: "POST",
+            headers: {"content-type": "application/json"},
+            body: JSON.stringify({
+                battery: Math.floor(50 + Math.random() * 51),
+                position_lat: Number(lat),
+                position_long: Number(long)
+            })
+        })
+        console.log(`Chargers -> ${city.name}`)
+
+    }
+}
+
 generateScooters()
+newParkings()
+newChargers()
