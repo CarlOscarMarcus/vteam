83ee661dca9195f663494f654f74415ff17dafb3
hhannaarvid <176487087+hhannaarvid@users.noreply.github.com>
2026-01-27 08:53:22 +0100

    lint fixes

--- frontend/web/src/context/UserContext.jsx
+++ frontend/web/src/context/UserContext.jsx
@@ -1,4 +1,4 @@
-import React, { Component }  from 'react';
+// import React, { Component }  from 'react';
 import PropTypes from 'prop-types';
 import { useContext, createContext, useState, useEffect } from "react";
 import { API_URL } from "../config";
